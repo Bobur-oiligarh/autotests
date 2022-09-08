6e04574ab9693844923fab28bf8de6ed2ceb803b
ivan <ivan.chepurin020396@gmail.com>
2022-09-08 15:41:58 +0500

    Добавил запрос Languages Добавил тип ответа AvailableLanguages запрос работает

--- api_mobile/tests/steps/references_steps.py
+++ api_mobile/tests/steps/references_steps.py
@@ -1,11 +1,19 @@
 import allure
 
 from api_mobile.requests.references.bankomates import Bancomates
+from api_mobile.requests.references.languages import Languages
 from utils.universal_steps.check_response import check_response
 
 
 @allure.step("Запрос банкоматов bankomates")
 def step_bankomates(client):
     response = Bancomates(client).response()
+    check_response(response, client)
+    response.data.set_data_to(client)
+
+
+@allure.step("Запрос доступных языков")
+def step_languages(client):
+    response = Languages().response()
     check_response(response, client)
     response.data.set_data_to(client)
