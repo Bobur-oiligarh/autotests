53af8fca241fc0afd0517e91a565fc98dc7d0f9f
bobur <burinboyev@hamkorlab.club>
2022-09-30 17:07:41 +0500

    изменения соответственно комментам

--- credentials_service/response_data_types/device_auth_data_type.py
+++ credentials_service/response_data_types/device_auth_data_type.py
@@ -10,8 +10,7 @@ class DeviceAuthDataType(AccRefTokens):
         self.identity_sign = data['identity_sign']
 
     def check(self, context, **kwargs):
-        self.assert_not_empty('access_token')
-        self.assert_not_empty('refresh_token')
+        super().check(context, **kwargs)
         self.assert_not_empty_int('identity_sign')
 
     def set_data_to(self, obj: CredentialServiceContext):
