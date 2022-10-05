e9f1b1927af93baf98681c2d48b8a18348c0ac73
B Urinboyev <burinboyev@hamkorlab.club>
2022-10-05 11:15:08 +0500

    реализовал таск VBNV-63 - limit_module: http://172.38.107.149:7081/operation/p2p-all

---
+++ limit_module/response_data_types/p2p_all_oper_limit_data_types.py
@@ -0,0 +1,32 @@
+from limit_module.test_data.limit_module_context import LimitModuleContext
+from utils.api_utils.response_data_base import BaseTypeParent, BaseType
+import allure
+
+
+class LimitDataTypes(BaseTypeParent):
+
+    def __init__(self, data: list):
+        super().__init__()
+        self.limits: list = self.deserialize_to_list_of(LimitDataType, data)
+
+    def check(self, context, **kwargs):
+        self.check_list_of("limits", context, **kwargs)
+
+    @allure.step("Устанавливаем лист лимитов к контексту")
+    def set_data_to(self, obj: LimitModuleContext):
+        self._set_data_to_product(obj)
+
+    def _set_data_to_product(self, obj: LimitModuleContext):
+        obj.limits = self
+
+
+class LimitDataType(BaseType):
+
+    def __init__(self, data: dict):
+        super().__init__()
+        self.type = data["Type"]
+        self.value = data["Value"]
+
+    def check(self, context, **kwargs):
+        self.assert_not_empty_str("type")
+        self.assert_not_empty_int("value")
