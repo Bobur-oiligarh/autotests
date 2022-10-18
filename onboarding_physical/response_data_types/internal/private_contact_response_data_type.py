b26f760606045d13831ecddb9fa69c75974cd137
bobur <burinboyev@hamkorlab.club>
2022-10-18 10:19:25 +0500

    исправил ошибку в private_contact_response_data_type

--- onboarding_physical/response_data_types/internal/private_contact_response_data_type.py
+++ onboarding_physical/response_data_types/internal/private_contact_response_data_type.py
@@ -1,19 +1,23 @@
 import allure
 from onboarding_physical.response_data_types.internal.private_prospects_data_type import PrivateProspectDataType
 from onboarding_physical.test_data.onboarding_physical_context import OnboardingPhysicalContext
+from utils.api_utils.response_data_base import BaseTypeParent
 
 
-class PrivateContactDataType(PrivateProspectDataType):
+class PrivateContactDataType(BaseTypeParent):
     def __init__(self, data: dict):
-        super().__init__(data=data)
-        self.phone_type = data['type']
+        super().__init__()
+        self.id = data['id']
+        self.prospect_id = data['prospect_id']
         self.phone = data['value']
-
+        self.phone_type = data['type']
+        self.verified_at = data['verified_at']
+        self.created_at = data['created_at']
 
     def check(self, context, **kwargs):
         self.assert_not_empty_str('id')
         self.assert_not_empty_str('prospect_id')
-        self.assert_not_empty_str('value')
+        self.assert_not_empty_str('phone')
         self.assert_not_empty_int('phone_type')
         self.assert_not_empty_str('verified_at')
         self.assert_not_empty_str('created_at')
