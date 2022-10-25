1667ce0600919f0724eaa43b5162752b37c32b75
bobur <burinboyev@hamkorlab.club>
2022-10-25 17:03:59 +0500

    закончил,

--- onboarding_physical/tests/test_cases/onboarding_physical_bobur_test.py
+++ onboarding_physical/tests/test_cases/onboarding_physical_bobur_test.py
@@ -26,20 +26,6 @@ class OnboardingPhysicalTC(TestCase):
             contact_id='0a699f19-d71b-48b0-8606-4837102c59c2',
             address_id='2056e105-703e-4d60-9af7-055ee56bbf56'
         )
-        self.context.private_address = PrivateAddress(
-            data={
-                'id': '2056e105-703e-4d60-9af7-055ee56bbf56',
-                'profile_id': '964b1484-00d4-4c22-b893-687743ded710',
-                'address': 'ТОШКЕНТ ШАҲРИ; ЯШНОБОД ТУМАНИ; г. Ташкент, Яшнабадский район, ул. Авиасозлар, пр. 1, '
-                           'Алимкент МСГ, 4- Дом, 10- Квартира',
-                'registered_at': '2021-05-04T00:00:00Z',
-                'country_code': '860',
-                'region_code': '26',
-                'district_code': '207',
-                'kadastr': '10:04:04:03:01:5005:0001:010',
-                'created_at': '2022-04-25T04:01:44.207872Z'
-            }
-        )
 
     def test_check_phone(self):
         step_check_phone(context=self.context)
--- utils/api_utils/test_request.py
+++ utils/api_utils/test_request.py
@@ -21,7 +21,7 @@ class RequestBase:
                  url: str,
                  method: str,
                  data_type: type = None,
-                 data: str = None,
+                 data: dict = None,
                  headers: dict = None,
                  cookies: dict = None,
                  parameters_in_list: bool = False,
@@ -75,7 +75,7 @@ class RequestBase:
         )
         self._response = TestResponse(response, self._data_type)
 
-    def _get_data(self) -> str:
+    def _get_data(self) -> json:
         """
         Сериализует поля без префикса "_" в параметры тела запроса,
         в качестве имени параметра будет использовано имя переменной
@@ -93,16 +93,19 @@ class RequestBase:
             data = self.__dict__[self._name_of_list]
         return json.dumps(data)
 
+    def data(self):
+        return self._data
+
 
 class TestRequest(RequestBase, ABC):
 
-    def _get_data(self) -> str:
-        if not self._parameters_in_list and super()._data:
-            data = super()._data
-            return json.dumps(data)
+    def _get_data(self) -> json:
+        # with allure.step(json.dumps(self.data())):
+        #     pass
+        # if self.data():
+        #     return json.dumps(self.data())
+        return super()._get_data()
 
     @allure.step("Отправляем запрос, получаем ответ")
     def _run(self):
         super()._run()
-
-
