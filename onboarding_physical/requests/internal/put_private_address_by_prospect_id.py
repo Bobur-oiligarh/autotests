f33687b9eb7da96b121ba3bf2847b10e27664d23
bobur <burinboyev@hamkorlab.club>
2022-10-25 12:30:57 +0500

    сделал VBNV 76

---
+++ onboarding_physical/requests/internal/put_private_address_by_prospect_id.py
@@ -0,0 +1,17 @@
+from onboarding_physical.response_data_types.internal.put_private_address_to_prospect_data_type import \
+    PutPrivateAddressToProspectDataType
+from onboarding_physical.test_data.onboarding_physical_context import OnboardingPhysicalContext
+from utils.api_utils.test_request import TestRequest
+from utils.api_utils.url_provider import URLProvider
+
+
+class PutPrivateAddressRequest(TestRequest):
+
+    def __init__(self, context: OnboardingPhysicalContext):
+        super().__init__(
+            URLProvider().url('onboarding_physical', f'private/address/{context.prospect_id}'),
+            'put',
+            data_type=PutPrivateAddressToProspectDataType,
+        )
+        self.private_address = context.private_address
+
