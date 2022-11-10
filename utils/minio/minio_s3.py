a2d6e0e80c113ab4c6fc137f5a9fe2f623a91a7b
bobur <burinboyev@hamkorlab.club>
2022-11-10 15:49:28 +0500

    1)реализовал загрузку файлов и фоток в отдельные бакеты 2) реализовал метод возврата данных в формате base64

--- utils/minio/minio_s3.py
+++ utils/minio/minio_s3.py
@@ -1,3 +1,5 @@
+import base64
+
 from minio.api import Minio
 from utils.patterns.singleton import Singleton
 from utils.yaml_reader import YAMLReader
@@ -6,61 +8,81 @@ from utils.yaml_reader import YAMLReader
 class MinioS3:
 
     def __init__(self):
-        self.bucket_name = YAMLReader().minio_data.get('bucket_name')
+        self.file_bucket = YAMLReader().minio_data.get('file_bucket')
+        self.image_bucket = YAMLReader().minio_data.get('image_bucket')
+        self.local_files_directory = YAMLReader().minio_data.get('file_folder_in_local')
+        self.local_images_directory = YAMLReader().minio_data.get('image_folder_in_local')
         self.client = Minio(
             endpoint=YAMLReader().minio_data.get('server'),
             access_key=YAMLReader().minio_data.get('access_key'),
             secret_key=YAMLReader().minio_data.get('secret_key'),
             secure=YAMLReader().minio_data.get('secure')
         )
-        self.file_folder = YAMLReader().minio_data.get('file_folder')
-        self.image_folder = YAMLReader().minio_data.get('image_folder')
 
-    def upload_file(self, file_name: str, file_path: str, content_type: str = "application/txt", **kwargs):
+    def upload_file(self, file_name: str, **kwargs):
+        """
+        Для загрузки файла в желаемом формате - передайти file_name с нужным расширением.(.txt, .yaml)
+        @param file_name: the files will be named in bucket (with extension)
+        @param kwargs:
+        @return:
+        """
+        self.client.fput_object(
+            bucket_name=self.file_bucket,
+            object_name=file_name,
+            file_path=f"{self.local_files_directory}/{file_name}",
+            **kwargs
+        )
+
+    def upload_image(self, image_name: str, **kwargs):
         """Для загрузки файла в желаемом формате - передайти file_name с нужным расширением.(.txt, .yaml)"""
-        self.client.fput_object(self.bucket_name,
-                                object_name=f"{self.file_folder}/{file_name}",
-                                file_path=f"{file_path}/{file_name}",
-                                content_type=content_type, **kwargs
-                                )
+        self.client.fput_object(
+            bucket_name=self.image_bucket,
+            object_name=image_name,
+            file_path=f"{self.local_images_directory}/{image_name}",
+            **kwargs
+        )
 
-    def upload_image(self, image_name: str, path_to_image: str, content_type: str = "application/png", **kwargs):
-        """Для загрузки файла в желаемом формате - передайти file_name с нужным расширением.(.txt, .yaml)"""
-        self.client.fput_object(self.bucket_name,
-                                object_name=f"{self.image_folder}/{image_name}",
-                                file_path=f"{path_to_image}/{image_name}",
-                                content_type=content_type, **kwargs
-                                )
-
-    def download_file(self, object_name: str, path_where_file_to_download: str, **kwargs):
+    def download_file(self, object_name: str, **kwargs):
+        """
+        @param object_name: name of the unloaded object
+        @param kwargs:
+        @return:
+        """
         try:
-            self.client.fget_object(bucket_name=self.bucket_name,
-                                    object_name=f"{self.file_folder}/{object_name}",
-                                    file_path=f"{path_where_file_to_download}/{object_name}", **kwargs
-                                    )
+            self.client.fget_object(
+                bucket_name=self.file_bucket,
+                object_name=object_name,
+                file_path=f"{self.local_files_directory}/{object_name}",
+                **kwargs
+            )
         except Exception as err:
             print(err)
 
-    def download_image(self, object_name: str, path_where_image_to_download: str, **kwargs):
+    def download_image(self, object_name: str, **kwargs):
         try:
-            self.client.fget_object(bucket_name=self.bucket_name,
-                                    object_name=f"{self.image_folder}/{object_name}",
-                                    file_path=f"{path_where_image_to_download}/{object_name}", **kwargs
-                                    )
+            self.client.fget_object(
+                bucket_name=self.image_bucket,
+                object_name=object_name,
+                file_path=f"{self.local_images_directory}/{object_name}",
+                **kwargs
+            )
         except Exception as err:
             print(err)
+
+    def data_to_base64(self, file_name):
+        image_formats = ['jpg', 'png', 'jpeg', 'svg', 'jfif', 'pjpeg', 'pjp']
+        file_format = file_name.split('.')[1]
+        if file_format in image_formats:
+            self.download_image(file_name)
+            file_path = f"{self.local_images_directory}/{file_name}"
+        else:
+            self.download_file(file_name)
+            file_path = f"{self.local_files_directory}/{file_name}"
+        with open(file_path, 'rb') as file:
+            encoded_string = base64.b64encode(file.read())
+            file.close()
+        return encoded_string.decode('utf-8')
 
 
 class MinioParent(MinioS3, metaclass=Singleton):
     pass
-
-
-if __name__ == '__main__':
-    pass
-    # m = MinioS3()
-    #
-    # print(os.path.abspath('hamkorautotests/requirements.txt'))
-    # m.upload_image('image.png', '/home/bobur/Pictures/Screenshot from 2022-09-14 14-09-45.png')
-    # m.download_image('image.png', '/home/bobur/Pictures/')
-    # m.upload_file('runner.py', '/home/bobur/PycharmProjects/hamkorautotests/')
-    # m.download_file('runner.py', '/home/bobur/')
