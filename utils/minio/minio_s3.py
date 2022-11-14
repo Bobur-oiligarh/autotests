033386378569618eeb35d24137cd959604a84053
ivan <ivan.chepurin020396@gmail.com>
2022-11-14 16:11:32 +0500

    Исправления

--- utils/minio/minio_s3.py
+++ utils/minio/minio_s3.py
@@ -10,8 +10,6 @@ class MinioS3:
     IMAGE_FORMATS = ['jpg', 'png', 'jpeg', 'svg', 'jfif', 'pjpeg', 'pjp']
 
     def __init__(self):
-        self.bucket_name = None
-        self.format = None
         self.client = Minio(
             endpoint=YAMLReader().minio_data.get('server'),
             access_key=YAMLReader().minio_data.get('access_key'),
@@ -22,40 +20,45 @@ class MinioS3:
     def put_object(self, object_name, data_in_bytes):
         data_as_a_stream = io.BytesIO(data_in_bytes)
         length = len(data_in_bytes)
-        self.format = object_name.split('.')[1]
-        self.bucket_name = "images" if self.format in self.IMAGE_FORMATS else "files"
-        self.client.put_object(bucket_name=self.bucket_name,
+        extension = object_name.split('.')[1]
+        self.client.put_object(bucket_name="images" if extension in self.IMAGE_FORMATS else "files",
                                object_name=object_name,
                                data=data_as_a_stream,
                                length=length)
 
     def get_object(self, object_name: str):
-        self.format = object_name.split('.')[1]
-        self.bucket_name = 'images' if self.format in self.IMAGE_FORMATS else 'files'
+        extension = object_name.split('.')[1]
+        bucket_name = 'images' if extension in self.IMAGE_FORMATS else 'files'
         obj = self.client.get_object(
-                     bucket_name=self.bucket_name,
+                     bucket_name=bucket_name,
                      object_name=object_name
                  )
-        if self.bucket_name == 'files':
-            decoded_object = obj.read().decode()
-        elif self.bucket_name == 'images':
-            decoded_object = Image.open(io.BytesIO(obj.read()))
-        return decoded_object
+        if bucket_name == 'files':
+            return obj.read().decode()
+
+        return Image.open(io.BytesIO(obj.read()))
+
 
     def to_base64(self, object_name: str):
         data = self.get_object(object_name)
-        if self.format not in self.IMAGE_FORMATS:
-            data_bytes = data.encode('utf-8')
-            base64_bytes = base64.b64encode(data_bytes)
-            result_b64_str = base64_bytes.decode('utf-8')
 
-        elif self.format in self.IMAGE_FORMATS:
-            buffered = io.BytesIO()
-            data.save(buffered, format=self.format)
-            buffered.seek(0)
-            img_byte = buffered.getvalue()
-            result_b64_str = f"data:image/{self.format};base64,{base64.b64encode(img_byte).decode()}"
-        return result_b64_str
+        extension = object_name.split(".")[1]
+        if extension in self.IMAGE_FORMATS:
+            return self._foto_to_b64(data, extension)
+
+        return self._str_to_b64(data)
+
+    def _str_to_b64(self, data):
+        data_bytes = data.encode('utf-8')
+        base64_bytes = base64.b64encode(data_bytes)
+        return base64_bytes.decode('utf-8')
+
+    def _foto_to_b64(self, data, extension):
+        buffered = io.BytesIO()
+        data.save(buffered, format=extension)
+        buffered.seek(0)
+        img_byte = buffered.getvalue()
+        return f"data:image/{extension};base64,{base64.b64encode(img_byte).decode()}"
 
 
 class MinioParent(MinioS3, metaclass=Singleton):
