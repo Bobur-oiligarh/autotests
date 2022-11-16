40d3ff1a7703ea949ebb0f65ff51a42c9d0dfaca
bobur <burinboyev@hamkorlab.club>
2022-11-16 11:33:00 +0500

    наименование классов изменил

--- card_service/requests/card_methods/get_p2p_info.py
+++ card_service/requests/card_methods/get_p2p_info.py
@@ -3,7 +3,7 @@ from utils.api_utils.test_request import TestRequest
 from utils.api_utils.url_provider import URLProvider
 
 
-class GetP2PInfoRequest(TestRequest):
+class GetP2PInfo(TestRequest):
     def __init__(self, context):
         super().__init__(
             url=URLProvider().url("card_service", f"v2/p2p-info/{context.card_number}"),
