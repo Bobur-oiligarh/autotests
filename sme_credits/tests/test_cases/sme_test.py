f7021d66eb7c786094d489e2b69d310ef03a817a
bobur <burinboyev@hamkorlab.club>
2022-11-17 14:47:55 +0500

    patch /accounts/{id} реализовал (2)

--- sme_credits/tests/test_cases/sme_test.py
+++ sme_credits/tests/test_cases/sme_test.py
@@ -1,7 +1,9 @@
 from unittest import TestCase
 
 from sme_credits.test_data.sme_context import SMEContext
+from sme_credits.tests.steps.account.delete_accounts_steps import step_delete_sme_accounts
 from sme_credits.tests.steps.account.get_accounts_steps import step_get_sme_accounts
+from sme_credits.tests.steps.account.patch_accounts_steps import step_patch_account
 from sme_credits.tests.steps.account.post_accounts_steps import step_post_sme_accounts
 
 
@@ -21,9 +23,10 @@ class SMETestCase(TestCase):
     # def test_post_sme_account(self):
     #     step_post_sme_accounts(self.context)
 
+    # def test_delete_sme_accounts(self):
+    #     step_delete_sme_accounts(self.context, account_id="d1aafa82-70eb-4528-bb63-49ebe67d3152")
+
     def test_patch_accounts(self):
         self.context.id = "fbadbe09-b336-452c-9b70-ec9a0d2907ee"
         step_patch_account(self.context)
 
-    def test_delete_sme_accounts(self):
-        step_delete_sme_accounts(self.context, account_id="d1aafa82-70eb-4528-bb63-49ebe67d3152")
