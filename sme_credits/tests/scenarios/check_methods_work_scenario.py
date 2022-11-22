d10bea74dc761fbc053499fb22f2a9a4d700c0e4
ivan <ivan.chepurin020396@gmail.com>
2022-11-22 11:40:16 +0500

    Добавил изменения в TestResponse

--- sme_credits/tests/scenarios/check_methods_work_scenario.py
+++ sme_credits/tests/scenarios/check_methods_work_scenario.py
@@ -8,16 +8,21 @@ from sme_credits.tests.steps.account.post_accounts_steps import step_post_sme_ac
 
 @allure.step("Проверка работы конечных точек SME accounts")
 def check_methods_work_scenario(context):
+    # запрос аккаунтов и  проверка отсутствия добавляемого в ответе
     step_get_sme_accounts(context)
+    context.accounts.account_not_exist(context.account)
+    # добавление нового аккаунта, проверка добавления
     step_post_sme_accounts(context)
     step_get_sme_accounts(context)
     context.accounts.account_exist(context.account.id)
+    # изменение аккаунта, проверка изменения
     context.account.change_param(param_name="account_mask", param_value="11111")
     step_patch_account(context)
     step_get_sme_accounts(context)
     context.account.assert_equal(
         context.accounts.get_account_by_param(param_name="id", param_value=context.account.id)
     )
+    # удаление аккаунта, проверка удаления
     step_delete_sme_accounts(context)
     step_get_sme_accounts(context)
     context.accounts.account_not_exist(context.account.id)
