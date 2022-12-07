19bfe2e581d18a343feececdd1459bcf9e778f8e
bobur <burinboyev@hamkorlab.club>
2022-12-07 04:11:04 -0500

    косметик изменения

--- sme_credits/tests/scenarios/strategy/check_strategy_methods_work_scenario.py
+++ sme_credits/tests/scenarios/strategy/check_strategy_methods_work_scenario.py
@@ -10,18 +10,23 @@ from sme_credits.tests.steps.strategy.step_post_strategy import step_post_strate
 
 @allure.step("Сценарий проверки работы классов на запросы POST, GET, GET ALL, PATCH, DELETE")
 def check_strategy_methods_work_scenario(context):
+    # Запрос всех объектов стратегии
     step_get_strategies(context)
 
+    # Запрос добавление новой стратегии, проверка добавления
     step_post_strategy(context)
     step_get_strategies(context)
     context.strategies.exist(param_name="id", param_value=context.strategy.id)
 
+    # Изменнение значения параметра у стратегии, проверка изменения
     context.strategy.change_param_value(param_name="product_id", param_value="11111")
     step_patch_strategy(context)
     step_get_strategies(context)
     context.strategy.check_objects_similarity(
         context.strategies.get_strategy_by_param(param_name="id", param_value=context.strategy.id)
     )
+
+    # Запрос на удаления стратегии, проверка удаления
     step_delete_strategy(context)
     step_get_strategies(context)
     context.strategies.not_exist(param_name="id", param_value=context.strategy.id)
